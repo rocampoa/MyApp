39d3600d5ceb1d3eebbb18eb14f357df7c119a94
Rafael Ocampo <rocampoa@gmail.com>
2017-07-28 00:10:13 -0500

    Se hace el primer commit sobre el proyecto de ejemplo de Angular2

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<h1> Mi Primera Aplicaci&oacute;n Angular 2 </h1>
+             <my-component></my-component>
+            <br/>
+  `
+    //'./app.component.html',
+
+})
+export class AppComponent {
+  title = 'My First Angular App Prueba';
+}
---
+++ src/app/my-component.component.ts
@@ -0,0 +1,36 @@
+import {Component} from '@angular/core';
+
+
+
+@Component({
+  selector: 'my-component',
+  template: `
+    <h1>Hola Mundo porque no lo toma {{name}}</h1>
+    <img [src]="imageUrl"/>
+    <br/>
+    <button [class.isActive] = "isActive" (click)="changeColor()" >Button</button>
+    <button [style.background-color] = "isActive ? 'red': 'blue'" (click)="changeMessage()">Button 2</button>
+    <h1>{{clickMessage}}</h1>
+    <br/>
+    <input type="text" [(ngModel)]="name"/>`,
+})
+
+export class MyComponent {
+   name;
+   imageUrl;
+   isActive;
+   clickMessage;
+   constructor() {
+     this.name = 'Rafael Ocampo Antero';
+     this.imageUrl = 'http://lorempixel.com/400/200/';
+     this.isActive = true;
+   }
+
+   changeMessage() {
+     this.clickMessage = 'Hola hizo click' + this.name;
+   }
+
+   changeColor() {
+     this.isActive = !this.isActive;
+   }
+}
